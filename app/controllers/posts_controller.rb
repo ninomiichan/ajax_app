8c89e6ab5f47cbcfb341fdcda9cbc99c625bb4bf
ninomiichan <michan_b_u@yahoo.co.jp>
2021-02-15 21:23:27 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,8 @@ class PostsController < ApplicationController
 end
 
   def create
-    Post.create(content: params[:content])
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
 end
 
